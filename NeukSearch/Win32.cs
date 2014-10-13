9056aba05876ab05235e9114ce867f57e8aced14
snowNymph <hong921122@nate.com>
2014-10-13 14:25:32 +0900

    win32 application 메뉴 긁어오기 추가

--- NeukSearch/Win32.cs
+++ NeukSearch/Win32.cs
@@ -12,7 +12,28 @@ namespace NeukSearch
         [DllImport("user32.dll")]
         public static extern IntPtr GetForegroundWindow();
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetMenu(IntPtr hWnd);
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetSubMenu(IntPtr hMenu, int nPos);
 
+        [DllImport("user32.dll")]
+        public static extern int GetMenuItemCount(IntPtr hMenu);
+
+        [DllImport("user32.dll")]
+        public static extern int GetMenuString(IntPtr hMenu, uint uIDItem, [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder lpString, int nMaxCount, uint uFlag);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern uint GetMenuItemID(IntPtr hMenu, int nPos);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        public const UInt32 MF_BYCOMMAND = 0x00000000;
+        public const UInt32 MF_BYPOSITION = 0x00000400;
     }
 }
