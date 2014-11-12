688cca79e65e8326f89a5ed0ac98f1fa8f60b4cf
snowNymph <hong921122@nate.com>
2014-11-13 00:41:14 +0900

    pinvoke 추가

--- NeukSearch/Win32.cs
+++ NeukSearch/Win32.cs
@@ -4,15 +4,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace NeukSearch
 {
     class Win32
     {
         [DllImport("user32.dll", SetLastError=true)]
-        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        public enum ShowWindowCommands : uint
+        {
+            SW_HIDE = 0,
+            SW_SHOWNORMAL = 1,
+            SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2,
+            SW_SHOWMAXIMIZED = 3,
+            SW_MAXIMIZE = 3,
+            SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5,
+            SW_MINIMIZE = 6,
+            SW_SHOWMINNOACTIVE = 7,
+            SW_SHOWNA = 8,
+            SW_RESTORE = 9
+        }
+
+        [DllImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+        #region processaccess
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+        #endregion
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+            ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int _GetWindowText(IntPtr hWnd,
+        StringBuilder lpWindowText, int nMaxCount);
 
 
-        
+        // GetClassName
+        [DllImport("user32.dll", EntryPoint = "GetClassName", ExactSpelling = false,
+            CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int _GetClassName(IntPtr hwnd, StringBuilder lpClassName,
+            int nMaxCount);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+             ProcessAccessFlags processAccess,
+             bool bInheritHandle,
+             int processId
+        );
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess(flags, false, proc.Id);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool QueryFullProcessImageName([In]IntPtr hProcess, [In]int dwFlags, [Out]StringBuilder lpExeName, ref int lpdwSize);
+
     }
 }
